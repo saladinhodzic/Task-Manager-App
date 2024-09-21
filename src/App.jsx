b86ef5bde4f8f3149e88d32a0b71a25cb27aad28
saladinhodzic <hsaladin06@gmail.com>
2024-09-21 16:59:30 +0200

    Building sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./components/Home";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <>
+    <main className=" h-screen my-8">
+      <Sidebar />
       <Home />
-    </>
+    </main>
   );
 }
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+export default function Sidebar() {
+  return (
+    <aside className="bg-stone-900 text-stone-50 rounded-r-md py-6 px-8 w-1/3 md:w-72">
+      <h2>Your projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
