e82b4e8d3200e33ee65954d60ae72362156b8fac
saladinhodzic <hsaladin06@gmail.com>
2024-09-22 13:35:33 +0200

    Styling sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,13 @@
 export default function Sidebar() {
   return (
     <aside className="bg-stone-900 text-stone-50 rounded-r-md py-6 px-8 w-1/3 md:w-72">
-      <h2>Your projects</h2>
+      <h2 className="font-bold uppercase text-stone-200 md:text-xl mb-8">
+        Your projects
+      </h2>
       <div>
-        <button>+ Add Project</button>
+        <button className="px-3 py-2 bg-stone-700 text-stone-400 rounded-md text-xs md:text-base hover:bg-stone-500 hover:text-stone-200">
+          + Add Project
+        </button>
       </div>
       <ul></ul>
     </aside>
