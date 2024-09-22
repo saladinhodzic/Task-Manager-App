b9f5470a4a058c34244ddb73033b25683341c865
saladinhodzic <hsaladin06@gmail.com>
2024-09-22 14:29:55 +0200

    Styling the Input fields components

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,14 @@
 export default function Input({ textarea, label, ...props }) {
+  const classes =
+    "w-full border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600 rounded-sm border-b-2 p-1";
   return (
-    <p>
+    <p className="flex flex-col ">
       <label>{label}</label>
-      {textarea ? <textarea {...props} /> : <input {...props} />}
+      {textarea ? (
+        <textarea className={classes} {...props} />
+      ) : (
+        <input className={classes} {...props} />
+      )}
     </p>
   );
 }
