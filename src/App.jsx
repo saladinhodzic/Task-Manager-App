55490070fc9fb3eea8e0b0ed052000bde7d46032
saladinhodzic <hsaladin06@gmail.com>
2024-09-22 14:15:05 +0200

    Adding new project component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./components/Home";
+import NewProject from "./components/NewProject";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <main className=" h-screen my-8">
+    <main className=" h-screen my-8 flex gap-8">
       <Sidebar />
-      <Home />
+      <NewProject />
+      {/* <Home /> */}
     </main>
   );
 }
