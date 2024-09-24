a503588819f4e87610544bf6c99926c692fb57cb
saladinhodzic <hsaladin06@gmail.com>
2024-09-24 21:57:28 +0200

    Adding error modal when input field validation is not met

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,8 +1,11 @@
 import { useRef } from "react";
 import Button from "./Button";
 import Input from "./Input";
+import Modal from "./Modal";
 
 export default function NewProject({ finishProject }) {
+  const dialog = useRef();
+
   const title = useRef();
   const desc = useRef();
   const date = useRef();
@@ -12,6 +15,15 @@ export default function NewProject({ finishProject }) {
     const enteredDesc = desc.current.value;
     const enteredDate = date.current.value;
 
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDesc.trim() === "" ||
+      enteredDate.trim() === ""
+    ) {
+      dialog.current.open();
+      return;
+    }
+
     finishProject({
       title: enteredTitle,
       desc: enteredDesc,
@@ -20,22 +32,28 @@ export default function NewProject({ finishProject }) {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <Button onClick={handleSave}>Save</Button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={title} label="Title" />
-        <Input ref={desc} label="Description" textarea />
-        <Input type="date" ref={date} label="Due Date" />
+    <>
+      <Modal ref={dialog} buttonCaption="Close">
+        <h2>You entered invalid input.</h2>
+        <p>Please enter a valid input.</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <Button onClick={handleSave}>Save</Button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={desc} label="Description" textarea />
+          <Input type="date" ref={date} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
